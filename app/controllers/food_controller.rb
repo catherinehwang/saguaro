6ba0c1e046c8abe0a619f6b28435485cb97d0563
Catherine Hwang <catherinemhwang@gmail.com>
2014-11-01 00:52:38 -0700

    Skeleton food list method

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -1,9 +1,19 @@
 class FoodController < ApplicationController
   def index
-    @money = params[:money]
+    money = params[:money]
 
+    food_list = calculate_food(money)
     respond_to do |format|
-        format.json { render json: @money, status: :ok }
+        format.json { render json: food_list, status: :ok }
+    end
+  end
+
+  def calculate_food(money)
+    # Assume Taco Bell for now
+    possible_food = Food.where(source: "Taco Bell")
+
+    return possible_food.map do |food| 
+      food.name
     end
   end
 end
