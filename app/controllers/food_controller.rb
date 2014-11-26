6b583df682c08b18f8719c85c1de9458da6059d4
Catherine Hwang <catherinemhwang@gmail.com>
2014-11-25 23:51:12 -0800

    Partially working knapsack

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -1,25 +1,34 @@
 class FoodController < ApplicationController
-  def index
-    money = params[:money]
-
-    food_list = calculate_food(money)
-    respond_to do |format|
-        format.json { render json: food_list, status: :ok }
-    end
-  end
-
   def calculate_food
-    money = params[:money]
+    money = params[:money].to_i
     source = params[:source]
 
-    possible_food = Food.where(source: source)
+    possible_food = Food.where(source: source).where.not(price: nil).where.not(calories: nil)
 
-    result = possible_food.map do |food|
-      food.name
-    end
+    result = determine_ratios(possible_food, money)
 
     respond_to do |format|
       format.json { render json: result, status: :ok }
     end
+  end
+
+  def determine_ratios(food, money)
+    cache = Array.new(food.length + 1, 0)
+    prev = Array.new(cache.length)
+    (1..money).each do |m|
+      new_weight = 0
+      food.each do |f|
+        if f.price > m
+          next
+        end
+        proposed = cache[m - f.price] + f.calories
+        if proposed > new_weight
+          cache[m] = proposed
+          new_weight = proposed
+          prev[m] = f
+        end
+      end
+    end
+    return prev[money].name
   end
 end
