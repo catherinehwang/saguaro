eee7cb142eb92af939f1a948795694dabbc874e0
Catherine Hwang <catherinemhwang@gmail.com>
2014-11-29 23:52:59 -0800

    Add working test

--- test/helpers/food_helper_test.rb
+++ test/helpers/food_helper_test.rb
@@ -5,6 +5,11 @@ class FoodHelperTest < ActionView::TestCase
   test "should knapsack properly" do 
     result = FoodHelper.determine_ratios(Food.all, 15)
 
-    assert_equal(result[:total_calories], 15)
+    food_quantities = result[:food_quantities]
+
+    assert_equal(food_quantities["Yellow"], 3)
+    assert_equal(food_quantities["Gray"], 3)
+
+    assert_equal(result[:total_calories], 36)
   end
 end
