1e4c7e6130d58b3fdea15300d7179942814924fc
Catherine Hwang <catherinemhwang@gmail.com>
2014-12-05 19:28:13 -0800

    Add code to determine what foods were chosen

--- app/helpers/food_helper.rb
+++ app/helpers/food_helper.rb
@@ -48,30 +48,63 @@ module FoodHelper
   module_function :determine_knapsack
 
   def knapsack_without_replacement(food, money)
+    # cache[m][f] : max calories of including foods 1..f with max money m
     cache = Array.new(money + 1)
     cache.each_with_index do |_, i|
       cache[i] = Array.new(food.length + 1, 0)
     end
 
-    prev = Array.new(money + 1)
-    prev.each_with_index do |_, i|
-      prev[i] = Array.new(food.length + 1)
+    # includes_matrix[m][f] : does the optimal solution with foods 1..f with max money m include item f?
+    includes_matrix = Array.new(money + 1)
+    includes_matrix.each_with_index do |_, i|
+      includes_matrix[i] = Array.new(food.length + 1)
     end
 
     (0..food.length-1).each do |f|
       (1..money).each do |m|
         if food[f].price > m
           cache[m][f] = cache[m][f-1]
+          includes_matrix[m][f] = true
         else
-          cache[m][f] = [
-            cache[m][f-1],
-            cache[m-food[f].price][f-1] + food[f].calories
-          ].max
+          not_include = cache[m][f-1]
+          include = cache[m-food[f].price][f-1] + food[f].calories
+
+          if include > not_include
+            cache[m][f] = include
+            includes_matrix[m][f] = true
+          else
+            cache[m][f] = not_include
+            includes_matrix[m][f] = false
+          end
         end
       end
     end
 
-    return cache[money][food.length - 1]
+
+    # Now we must determine which items were involved in the optimal solution
+
+    items_taken_status = Array.new(food.length, false)
+    money_left = money
+
+    (0..food.length).each do |f|
+      if includes_matrix[money_left][f]
+        items_taken_status[f] = true
+        money_left -= food[f].price
+      end
+    end
+
+    food_quantities = {}
+
+    items_taken_status.each_with_index do |status, index|
+      if status
+        food_quantities[food[index].name] = 1
+      end
+    end
+
+    return {
+      total_calories: cache[money][food.length - 1],
+      food_quantities: food_quantities
+    }
 
   end
   module_function :knapsack_without_replacement
--- test/helpers/food_helper_test.rb
+++ test/helpers/food_helper_test.rb
@@ -16,6 +16,13 @@ class FoodHelperTest < ActionView::TestCase
   test "should knapsack without replacement properly" do
     result = FoodHelper.knapsack_without_replacement(Food.all, 15)
 
-    assert_equal(15, result)
+    assert_equal(15, result[:total_calories])
+
+    food_quantities = result[:food_quantities]
+    assert_equal(1, food_quantities["Yellow"])
+    assert_equal(1, food_quantities["Red"])
+    assert_equal(1, food_quantities["Blue"])
+    assert_equal(1, food_quantities["Green"])
+    assert_equal(nil, food_quantities["Gray"])
   end
 end
