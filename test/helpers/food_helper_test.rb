19ecb4541a3d967555440feaa0ae1c6ae90ccf74
Catherine Hwang <catherinemhwang@gmail.com>
2014-12-10 13:53:25 -0800

    Add test case from Papadimitriou's algorithms book

--- test/helpers/food_helper_test.rb
+++ test/helpers/food_helper_test.rb
@@ -13,6 +13,16 @@ class FoodHelperTest < ActionView::TestCase
     assert_equal(result[:total_calories], 36)
   end
 
+  test "should knapsack with replacement properly" do
+    result = FoodHelper.knapsack_with_replacement(Food.where(source: "museum"), 10)
+
+    food_quantities = result[:food_quantities]
+
+    assert_equal(48, result[:total_calories])
+    assert_equal(1, food_quantities["One"][:quantity])
+    assert_equal(2, food_quantities["Four"][:quantity])
+  end
+
   test "should knapsack without replacement properly" do
     result = FoodHelper.knapsack_without_replacement(Food.where(source: "box"), 15)
 
@@ -26,5 +36,17 @@ class FoodHelperTest < ActionView::TestCase
     assert_equal(1, food_quantities["Blue"][:quantity])
     assert_equal(1, food_quantities["Gray"][:quantity])
     assert_equal(nil, food_quantities["Green"])
+  end
+
+  test "should knapsack without replacement again" do
+    result = FoodHelper.knapsack_without_replacement(Food.where(source: "museum"), 10)
+
+    food_quantities = result[:food_quantities]
+
+    assert_equal(46, result[:total_calories])
+    assert_equal(1, food_quantities["One"][:quantity])
+    assert_equal(nil, food_quantities["Two"][:quantity])
+    assert_equal(1, food_quantities["Three"][:quantity])
+    assert_equal(nil, food_quantities["Four"][:quantity])
   end
 end
